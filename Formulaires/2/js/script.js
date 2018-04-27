337c5ffa0651ff3782de610b45155b8c9e22e61d
Petit <33902879+petit-louise@users.noreply.github.com>
2018-04-27 14:20:16 +0200

    Add files via upload

---
+++ Formulaires/2/js/script.js
@@ -0,0 +1,38 @@
+﻿"use strict";
+
+//Déclaration des variables
+
+
+
+
+//Déclaration des fonctions
+
+function wordCounter(targetElement, targetFrame, maxWord) 
+{
+	let frames=document.querySelectorAll(targetFrame);
+	let targetField=document.querySelector(targetElement);
+	targetField.addEventListener("keyup", function()
+	{
+		let comment=targetField.value;
+		let nbWords=comment.split(" "); //on récupère un tableau de mots
+		let nbCharacters=comment.length; //on compte les caractères
+		frames[0].innerText=nbWords.length; //on compte les mots
+		frames[2].innerText=nbCharacters;
+
+		if (comment=="")
+		{
+			frames[0].innerText=0;
+		}
+		if (frames[0].innerText>maxWord)
+		{
+			targetField.setAttribute("maxlength", nbCharacters);
+		}
+	});
+	frames[1].innerText=maxWord;
+}
+
+//Activation des fonctions
+document.addEventListener("DOMContentLoaded", function()
+{
+	wordCounter("#comment", ".frame", 5);
+});
